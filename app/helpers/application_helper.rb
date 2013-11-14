0151a09bfc2a688ca4feb28217f576a170164096
thib thib <thibthib@thib-thibs-MacBook.local>
2013-11-14 12:29:07 +0100

    Fin du layout et des routages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  # logo
+  def logo
+	  logo = image_tag("logo.png", :alt => "Application exemple", :class => "round")
+  end
+
+
+  # Retourner un titre basé sur la page.
+  def title
+    base_title = "Simple App du Tutoriel Ruby on Rails"
+    if @title.nil?
+      base_titre
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
