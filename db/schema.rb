728e7d8d86c3fbfefbb134e60456872bf459533a
thib thib <thibthib@thib-thibs-MacBook.local>
2013-12-12 22:05:52 +0100

    dede3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131120190919) do
+ActiveRecord::Schema.define(:version => 20131212203416) do
 
   create_table "users", :force => true do |t|
     t.string   "nom"
